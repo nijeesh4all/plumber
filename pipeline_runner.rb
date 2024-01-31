88ed8d4fbacff0f17d9ae0dd0d1da3c5fbc83cc0
Nijeesh Joshy <nijeesh4all@gmail.com>
2024-01-31 11:43:54 +0530

    PROT-1354 #comment [Rubocop] fixes

--- pipeline_runner.rb
+++ pipeline_runner.rb
@@ -2,6 +2,14 @@
 
 require 'yaml'
 
+# The PipelineRunner class is responsible for loading, configuring, and running data processing pipelines.
+# It reads pipeline configurations from YAML files located in the './pipelines/' directory.
+# Pipelines can be selectively executed based on the provided list of pipeline names.
+# The class encapsulates the logic for running each pipeline, measuring execution time, and logging information.
+#
+# @example
+#   runner = PipelineRunner.new(['pipeline1', 'pipeline2'])
+#   runner.run_pipelines
 class PipelineRunner
   def initialize(pipelines_to_run = PipelineRunner.all_pipeline_names)
     @pipelines_to_run = pipelines_to_run
@@ -21,7 +29,7 @@ class PipelineRunner
     Dir.glob('./pipelines/*.yml').each do |file|
       loaded_config = YAML.load(File.read(file), symbolize_names: true)
       pipeline_name = File.basename(file, '.yml')
-      configurations[pipeline_name] =  loaded_config
+      configurations[pipeline_name] = loaded_config
     end
 
     configurations
@@ -33,14 +41,23 @@ class PipelineRunner
     configurations = PipelineRunner.load_pipeline_configurations
 
     configurations.each do |pipeline_name, pipeline_config|
-      @logger.info "#{"=" * 50} PIPELINE LOADED #{pipeline_name} #{"=" * 50}"
-      started_time = Time.now
-      next if @pipelines_to_run.any? && !@pipelines_to_run.include?(pipeline_name)
-      pipeline_factory = Pipelines::Factory.new(pipeline_config[0])
-      Kiba.run(pipeline_factory.pipeline)
-      @logger.info "total time to sync #{Time.now - started_time}s"
-      @logger.info "#{"=" * 50} PIPELINE RAN #{pipeline_name} #{"=" * 50}"
+      run_pipeline(pipeline_name, pipeline_config) unless skip_pipeline?(pipeline_name)
     end
   end
-end
 
+  def run_pipeline(pipeline_name, pipeline_config)
+    @logger.info "#{'=' * 50} PIPELINE LOADED #{pipeline_name} #{'=' * 50}"
+
+    started_time = Time.now
+    pipeline_factory = Pipelines::Factory.new(pipeline_config[0])
+    Kiba.run(pipeline_factory.pipeline)
+
+    @logger.info "total time to sync #{Time.now - started_time}s"
+    @logger.info "#{'=' * 50} PIPELINE RAN #{pipeline_name} #{'=' * 50}"
+  end
+
+  def skip_pipeline?(pipeline_name)
+    @pipelines_to_run.any? && !@pipelines_to_run.include?(pipeline_name)
+  end
+
+end
