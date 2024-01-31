d52abbe95072a5d883faae615334e9173f9ce687
Nijeesh Joshy <nijeesh4all@gmail.com>
2024-01-31 12:18:00 +0530

    PROT-1354 #comment [Rubocop] fixes

--- src/transforms/value_copy.rb
+++ src/transforms/value_copy.rb
@@ -1,6 +1,17 @@
 # frozen_string_literal: true
 
 module Transforms
+  # The ValueCopy class represents a transformation that copies values from source columns to destination columns.
+  # It takes a hash of column mappings during initialization and applies these mappings to copy values.
+  #
+  # Usage:
+  #   - Initialize with a hash of column mappings specifying source and destination columns.
+  #   - Call the 'process' method to apply the value copying transformation to a data row.
+  #
+  # Example:
+  #   value_copy_transform = Transforms::ValueCopy.new(columns_map: { source_column1: :destination_column1,
+  #                                                                   source_column2: :destination_column2 })
+  #   transformed_row = value_copy_transform.process({ source_column1: 'value1', source_column2: 'value2' })
   class ValueCopy
     def initialize(columns_map: {})
       @columns_map = columns_map
@@ -8,11 +19,9 @@ module Transforms
 
     def process(row)
       @columns_map.each do |source_column, destination_column|
-
         row[destination_column.to_s] = row[source_column.to_sym]
       end
       row
     end
   end
-
 end
