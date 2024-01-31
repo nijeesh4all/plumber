42670f8981818825fecb4c50013f6ee0347355e5
Nijeesh Joshy <nijeesh4all@gmail.com>
2024-01-31 11:52:27 +0530

    PROT-1354 #comment [Rubocop] fixes

--- lib/pipeline_logger.rb
+++ lib/pipeline_logger.rb
@@ -1,13 +1,16 @@
 # frozen_string_literal: true
+
 require 'singleton'
 require 'semantic_logger'
+# simple Logger class that wraps SemanticLogger
 class PipelineLogger
   include Singleton
 
   attr_reader :logger
+
   def initialize
-    SemanticLogger.add_appender(io:STDOUT)
-    @logger = SemanticLogger["Plumber"]
+    SemanticLogger.add_appender(io: $stdout)
+    @logger = SemanticLogger['Plumber']
     SemanticLogger.default_level = :trace
   end
 
@@ -22,5 +25,4 @@ class PipelineLogger
   def respond_to_missing?(method_name, include_private = false)
     logger.respond_to?(method_name) || super
   end
-
 end
