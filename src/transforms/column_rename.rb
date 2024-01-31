fd424c9b668a69a00aa61454832d47024ee9b512
Nijeesh Joshy <nijeesh4all@gmail.com>
2024-01-31 12:17:01 +0530

    PROT-1354 #comment [Rubocop] fixes

--- src/transforms/column_rename.rb
+++ src/transforms/column_rename.rb
@@ -1,6 +1,17 @@
 # frozen_string_literal: true
 
 module Transforms
+  # The ColumnRename class represents a transformation that renames columns in a data row based on a provided mapping.
+  # It takes a hash of column mappings during initialization and applies these mappings to rename the columns.
+  #
+  # Usage:
+  #   - Initialize with a hash of column mappings.
+  #   - Call the 'process' method to apply the column renaming transformation to a data row.
+  #
+  # Example:
+  #   column_rename_transform = Transforms::ColumnRename.new(columns_map: { old_name1: :new_name1,
+  #                                                                           old_name2: :new_name2 })
+  #   transformed_row = column_rename_transform.process({ old_name1: 'value1', old_name2: 'value2' })
   class ColumnRename
     def initialize(columns_map: {})
       @columns_map = columns_map
@@ -16,5 +27,4 @@ module Transforms
       new_row
     end
   end
-
 end
